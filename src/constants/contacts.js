4ce7d10c870f7a2e720fb12c39f45c25cdd89115
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-05-15 00:01:52 +0200

    hw

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,10 +1,3 @@
 import path from 'node:path';
 
-const pathToWorkDir = path.join(process.cwd(), 'src');
-export const PATH_DB = path.join(
-  pathToWorkDir,
-  'src',
-  'scripts',
-  'db',
-  'db.json',
-);
+export const PATH_DB = path.join('src', 'db', 'db.json');
--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,8 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { getAllContacts } from './getAllContacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  const contactsQuantity = (await getAllContacts()).length;
+  return contactsQuantity;
+};
 
 console.log(await countContacts());
